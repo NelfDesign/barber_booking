22bd758cde0d0b458d2dd19aa2185156911ea192
Nelfdesign <fabrice.design85@gmail.com>
2021-05-24 09:21:14 +0200

    add salon selection UI

--- lib/cloud_firestore/all_salon_ref.dart
+++ lib/cloud_firestore/all_salon_ref.dart
@@ -1,4 +1,5 @@
 import 'package:barber_booking/model/city_model.dart';
+import 'package:barber_booking/model/salon_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<List<CityModel>> getCities() async {
@@ -7,6 +8,17 @@ Future<List<CityModel>> getCities() async {
   QuerySnapshot snapshot = await cityRef.get();
   snapshot.docs.forEach((element) {
     result.add(CityModel.fromJson(element.data()));
+  });
+  return result;
+}
+
+Future<List<SalonModel>> getSalonByCity(String cityName) async {
+  List<SalonModel> result = new List<SalonModel>.empty(growable: true);
+  CollectionReference salonRef = FirebaseFirestore.instance.collection('AllSalon')
+      .doc(cityName.replaceAll(' ', '')).collection("Branch");
+  QuerySnapshot snapshot = await salonRef.get();
+  snapshot.docs.forEach((element) {
+    result.add(SalonModel.fromJson(element.data()));
   });
   return result;
 }
